e535445b5e118bb7ed2cef499cbe42c90352f22d
srtmaks <sir.maxym@gmail.com>
2022-08-02 16:54:33 +0300

    Publishing App

---
+++ src/OperationButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { ACTIONS } from "./App";
+
+export function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
